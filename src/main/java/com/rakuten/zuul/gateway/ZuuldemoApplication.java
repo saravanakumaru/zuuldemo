71265fc5d362eada4e497b40099fcdf852c2271a
Saravana <saravana.kumar@rakuten.com>
2018-04-26 13:57:50 +0800

    Initial release for zuul

---
+++ src/main/java/com/rakuten/zuul/gateway/ZuuldemoApplication.java
@@ -0,0 +1,12 @@
+package com.rakuten.zuul.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuuldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuuldemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rakuten/zuul/gateway/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.rakuten.zuul.gateway.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/")
+public class HomeController {
+	private static Logger log = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "home", method = RequestMethod.GET)
+	public String getHomePage() {
+		return "Welcome to Spring boot app";
+	}
+
+	@RequestMapping(value = "campaign", method = RequestMethod.GET)
+	public String getCampaignDetails() {
+		log.info("This >> Coming in to the campaign");
+		return "This is inside Campaign details";
+	}
+
+}
