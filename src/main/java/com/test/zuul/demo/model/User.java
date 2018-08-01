d9a11ef5ab8a2d14bcf125ec738e46cd2d75787f
Saravana <saravana.kumar@rakuten.com>
2018-08-01 18:25:22 +0800

    config changes

---
+++ src/main/java/com/test/zuul/demo/model/User.java
@@ -0,0 +1,13 @@
+package com.test.zuul.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+@Getter @Setter 
+public class User {
+
+	private Long id;
+	private String name;
+	private String dob;
+}
